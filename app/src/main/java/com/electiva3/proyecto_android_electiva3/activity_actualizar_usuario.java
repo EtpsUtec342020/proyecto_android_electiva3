e8c54c52c0b1cb7c6c6cce7504c0f66939938e91
ViktorG04 <61758307+ViktorG04@users.noreply.github.com>
2020-09-03 23:32:59 -0600

    usuario

---
+++ app/src/main/java/com/electiva3/proyecto_android_electiva3/activity_actualizar_usuario.java
@@ -0,0 +1,14 @@
+package com.electiva3.proyecto_android_electiva3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_actualizar_usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_usuario);
+    }
+}
---
+++ app/src/main/java/com/electiva3/proyecto_android_electiva3/activity_new_usuario.java
@@ -0,0 +1,16 @@
+package com.electiva3.proyecto_android_electiva3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.electiva3.proyecto_android_electiva3.R;
+
+public class activity_new_usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_usuario);
+    }
+}
