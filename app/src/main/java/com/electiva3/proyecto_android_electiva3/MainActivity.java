19380558a88dc1fc5c21cf92a4a8114f4ae7716d
jorge9797 <32504262+jorge9797@users.noreply.github.com>
2020-09-20 19:22:42 -0600

    Login firebase correo y contraseña

--- app/src/main/java/com/electiva3/proyecto_android_electiva3/MainActivity.java
+++ app/src/main/java/com/electiva3/proyecto_android_electiva3/MainActivity.java
@@ -1,18 +1,33 @@
 package com.electiva3.proyecto_android_electiva3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity
 {
-    private TextInputLayout edtCorreo, edtContrasena;
+    private EditText edtCorreo, edtContrasena;
     private Button btnIngresar;
+
+
+    FirebaseAuth auth;
+    DatabaseReference database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,15 +36,49 @@ public class MainActivity extends AppCompatActivity
 
         getSupportActionBar().hide();
 
+        edtCorreo =  findViewById(R.id.edtCorreo);
+        edtContrasena =  findViewById(R.id.edtContrasena);
+
         btnIngresar = findViewById(R.id.btnRegistrar);
+
+        auth = FirebaseAuth.getInstance();
+        database  = FirebaseDatabase.getInstance().getReference();
 
         btnIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), activity_principal.class);
-                startActivity(i);
-                finish();
+                login();
             }
         });
     }
+
+    private void login(){
+
+        String email =  edtCorreo.getText().toString();
+        String password =  edtContrasena.getText().toString();
+
+        if(email.isEmpty()){
+            edtCorreo.setError("Compo requerido");
+        }else  if(password.isEmpty()){
+            edtContrasena.setError("Compo requerido");
+        }else{
+            //Toast.makeText(getApplicationContext(), "Login is successfull", Toast.LENGTH_SHORT).show();
+            auth.signInWithEmailAndPassword(email , password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if(task.isSuccessful()){
+
+                        Intent i = new Intent(getApplicationContext(), activity_principal.class);
+                        startActivity(i);
+                        finish();
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Contraseña o usuario incorrectos", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
+
 }
